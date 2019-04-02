8078860094663d6d3c4c291a5a9d996aba9522ee
Mikita Luksha <nikita.luksha@gmail.com>
2019-04-02 07:57:52 +0300

    Init routes

---
+++ articles.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { titile: 'test1', text: 'text 1' },
+  { titile: 'test2', text: 'text 2' },
+  { titile: 'test3', text: 'text 3' },
+  { titile: 'test4', text: 'text 4' },
+];
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const articles = require('./articles');
+
 const port = process.env.PORT || 3000;
 
 
@@ -9,6 +11,29 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
+app.get('/articles', (req, res, next) => {
+  res.send(articles);
+});
+
+app.post('/articles', (req, res, next) => {
+  res.send('Ok');
+});
+
+app.get('/articles/:id', (req, res, next) => {
+  const { id } = req.params;
+  res.send(articles[id]);
+});
+
+app.delete('/articles/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  delete articles[id];
+
+  res.send({ message: 'Deleted' });
+});
+
 app.listen(port, () => {
   console.log(`Express localhost:${port}`);
 });
+
+module.exports = app;
