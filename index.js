b7c90609b45501e6473efdcd6a0b2cc2c244398f
Mikita Luksha <nikita.luksha@gmail.com>
2019-04-02 07:24:50 +0300

    add eslint

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+  res.send('Hello world');
 });
 
 app.listen(port, () => {
-    console.log(`Express localhost:${port}`);
+  console.log(`Express localhost:${port}`);
 });
